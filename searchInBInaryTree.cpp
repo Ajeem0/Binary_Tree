8f22c9374a19ae10050e95891171dda4cb678b23
Ajeem0 <azeemsilawat573@gmail.com>
2025-04-17 08:34:57 +0530

    new code

---
+++ searchInBInaryTree.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class BinaryTreeNode{
+public:
+int data;
+BinaryTreeNode* left;
+BinaryTreeNode* right;
+BinaryTreeNode(int val){
+    data = val;
+    left = nullptr;
+    right = nullptr;
+}
+static bool findInBT(BinaryTreeNode* root,int data){
+    if(root == nullptr)
+       return false;
+    if(root->data == data)
+        return true;
+    return findInBT(root->left,data) || findInBT(root->right,data);
+    
+}
+
+};
+
+int main(){
+    BinaryTreeNode* root = new BinaryTreeNode(10);
+    root->left = new BinaryTreeNode(2);
+    root->right = new BinaryTreeNode(6);
+    root->left->right = new BinaryTreeNode(4);
+    root->left->left = new BinaryTreeNode(8);
+    
+    int key = 8;
+    if(BinaryTreeNode::findInBT(root,key))
+        cout<<key<<" Find in Binary Tree";
+    else
+        cout<<key<<" is Not found";
+
+    return 0;
+}
